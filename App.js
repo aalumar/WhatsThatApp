77b0828579fa6ab8920730870b6f35601432510e
aalumar <alumarabdullah@gmail.com>
2023-03-09 20:09:02 +0000

    Tab navigator implemented for chats, contacts, and profile screens. HomeScreen changed to ChatsScreen. Stack navigator rearranged to be included in own file.

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './components/login-component';
-import RegisterScreen from './components/register-component';
-import HomeScreen from './components/home-screen-component';
+import StackNavigator from './navigation/main-stack-navigator';
 
 class App extends Component {
 
   render () {
 
-    const Stack = createNativeStackNavigator();
-
     return (
       
-      <NavigationContainer>
-
-        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
-
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          
-        </Stack.Navigator>
-
-      </NavigationContainer>
+      <StackNavigator />
       
     );
   }
